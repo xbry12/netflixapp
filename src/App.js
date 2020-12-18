77728a41026a3c615ce2508832a66bb5d8539433
Bryant Pyram <bryant.pyram@gmail.com>
2020-12-18 01:26:04 -0500

    ADDED CRUD IN LESS THAN 12 HOURS THANKS TO DHRUV THE BEST ENGINEER IN THE WORLD

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
+import React, {Component} from 'react';
 import "./App.css";
 import Nav from "./Nav";
 import Home from "./Home";
 import SignIn from "./SignIn";
 import Movies from "./Movies";
-import Profile from "./profile"
+import Profile from "./profile";
 import { Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="app">
-      <Nav />
-      {/* remove nav */}
-      <Switch>
-        <Route path="/home" component={Home} />
-        <Route path="/signin" component={SignIn} />
-        <Route path="/movies" component={Movies} />
-        <Route path="/profile" component={Profile} />
-      </Switch>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+  }
+  // componentDidMount() {
+  //   fetch("http://localhost:8000/profile/1", {
+  //     method: "GET",
+  //     headers: {
+  //       Accept: "application/json",
+  //     },
+  //   })
+  //   .then((response) => response.json())
+  //   .then((output) => console.log({output}));
+  // }
+  render() {
+    return (
+      <div className="app">
+        <Nav />
+        {/* remove nav */}
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/signin" component={SignIn} />
+          <Route path="/movies" component={Movies} />
+          <Route path="/profile" component={Profile} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/profile.js
+++ src/profile.js
@@ -35,45 +35,130 @@ class Profile extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      profiles
+      profile: [],
+      photo_url: ""
     };
   }
-
-//   component did mount 
-
-  handleDelete = () => {
+  componentDidMount() {
     fetch("http://localhost:8000/profile/", {
+      method: "GET",
       headers: {
         Accept: "application/json",
-        "Access-Control-Allow-Origin":"*"
       },
+    })
+      .then((response) => response.json())
+      .then((output) => this.setState({ profile: output }));
+  }
+//   handleAcc = () => {
+//     // e.preventDefault();
+
+//     fetch("https://localhost:8000/profile/", {
+//       method: "POST",
+//       headers: {
+//         Accept: "application/json",
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ "name": "Bryant", "photo_url": "https://static01.nyt.com/images/2020/02/02/sports/02kobe-essay-lede/merlin_93708212_ff16a83c-391b-4b25-a4d5-77eaf0cbbb20-superJumbo.jpg" }),
+//     })
+//       .then((response) => response.json())
+//       .then((output) => console.log(output));
+//   };
+
+handleAcc = () => {
+    console.log("here")
+    fetch("http://localhost:8000/profile/", {
+      method: "POST", 
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({"name":"bryant", "photo_url":"google.com"}),
     })
       .then((response) => response.json())
       .then((output) => console.log(output));
   };
-//   this.setState({profile: output })
 
+  handleClass = (e) => {
+    this.setState({
+      photo_url: e.target.value,
+    });
+    console.log(e.target.value);
+  };
+
+  handleDelete = (pk) => {
+    console.log("here");
+    console.log(pk);
+    fetch("http://localhost:8000/profile/" + pk, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: "bryant", photo_url: "google.com" }),
+    })
+      .then((response) => response.json())
+      .then((output) => console.log(output));
+  };
+  handleUpdate = () => {
+    console.log("here")
+    fetch("http://localhost:8000/profile/5", {
+      method: "PUT", 
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({name:"Hello", "photo_url":"google.com"}),
+    })
+      .then((response) => response.json())
+      .then((output) => console.log(output));
+  };
+
+  // read
   render() {
+    console.log(this.state.profile);
     return (
       <div className="profiles-container">
         <span className="title">Who's watching?</span>
-        <div className="profiles">
-          {profiles.map((profile, index) => (
-            <div
-              key={index}
-              onClick={(e) => console.log(profile)}
-              className="profile"
-            >
-              <img className="image" src={profile.image_src} />
-              <span className="username">{profile.username}</span>
-            </div>
-          ))}
-        </div>
-        <button>manage profile</button>
+        {this.state.profile ? (
+          <div className="profiles">
+            {this.state.profile.map((profile, index) => (
+              <div
+                key={index}
+                onClick={(e) => console.log(profile)}
+                className="profile"
+              >
+                <img className="image" src={profile.photo_url} />
+                <span className="username">{profile.name}</span>
+                <button onClick={() => this.handleDelete(index)}>
+                  {" "}
+                  Delete{" "}
+                </button>
+              </div>
+            ))}
+          </div>
+        ) : (
+          "loading"
+        )}
+
+        <button onClick={() => this.handleAcc()}>manage profile</button>
         <button className="delete" onClick={() => this.handleDelete()}>
           delete profile
         </button>
 
+        <form>
+          <label>
+            Choose Day
+            <input type="text" onChange={this.handleClass} name="name" />
+          </label>
+          <div>
+            <button
+              className=""
+              label="updateimg"
+              onClick={() => this.handleUpdate()}
+            >Update Image</button>
+          </div>
+        </form>
+    
         {/* <Button ghost secondary label="MANAGE PROFILES" /> */}
       </div>
     );
